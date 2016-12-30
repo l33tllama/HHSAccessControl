41e08b8b5753d6f6e5e20cf4c942675492bd478f
Leo <overclockedamiga@gmail.com>
2016-12-30 18:53:53 +1100

    text

--- sqlite3db_init.sh
+++ sqlite3db_init.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-cd "${0%/*}"
-sqlite3 ControlPanel.py < init.sql
+sqlite3 ControlPanel.py 'CREATE TABLE access_list(
+rfid INTEGER PRIMARY KEY,
+name TEXT,
+id INTEGER,
+phone INTEGER,
+end_date DATE NOT NULL,
+alarm INTEGER,
+access INTEGER);'
 python ControlPanel.py
