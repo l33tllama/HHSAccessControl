41cd32732b5eb1b1c62c1a8b725ce9c08b116a2e
Leo <overclockedamiga@gmail.com>
2016-12-30 08:02:34 +0000

    Fixed sqlite init..

--- sqlite3db_init.sh
+++ sqlite3db_init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sqlite3 ControlPanel.py "CREATE TABLE access_list(rfid INTEGER PRIMARY KEY,name TEXT,id INTEGER,phone INTEGER,end_date DATE NOT NULL,alarm INTEGER,access INTEGER);"
+echo "CREATE TABLE access_list(rfid INTEGER PRIMARY KEY,name TEXT,id INTEGER,phone INTEGER,end_date DATE NOT NULL,alarm INTEGER,access INTEGER);" | sqlite3 ControlPanel.db
 python ControlPanel.py
