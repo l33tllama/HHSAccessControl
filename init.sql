a3f85e5e2083dbca2811adfd4dea3395c50a3856
Craig Marshall <craig9@gmail.com>
2017-01-02 10:07:36 +0000

    Fixed data types for Rfid and Phone in SQL init script. Moved not needed files to NotNeeded. Added a .gitignore file. Fixed sqlite3db_init.sh to use correct SQL init script

--- init.sql
+++ init.sql
@@ -2,7 +2,7 @@ CREATE TABLE access_list(
 rfid INTEGER PRIMARY KEY,
 name TEXT,
 id INTEGER,
-phone INTEGER,
+phone TEXT,
 end_date DATE NOT NULL,
 alarm INTEGER,
 access INTEGER);
--- sqlite3db_init.sh
+++ sqlite3db_init.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-echo "CREATE TABLE access_list(rfid INTEGER PRIMARY KEY,name TEXT,id INTEGER,phone INTEGER,end_date DATE NOT NULL,alarm INTEGER,access INTEGER);" | sqlite3 ControlPanel.db
+
+# Just in case
+rm ControlPanel.db.old
+mv ControlPanel.db ControlPanel.db.old
+
+cat init.sql | sqlite3 ControlPanel.db
 python ControlPanel.py
+
